de0288b0cd6ca355dc473ac776c63fa922b7b9fa
Stirling Hostetter <stirhoss@icloud.com>
2022-04-06 12:13:42 -0500

    Add test for reading name and age

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,15 @@ require 'pry'
 
 class Student
 
-  def initialize(name, age)
-    @name = name
-    @age = age
+  def initialize(student_info)
+    @student_info = student_info
+  end
+
+  def name
+    @student_info[:name]
+  end
+
+  def age
+    @student_info[:age]
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,6 +6,13 @@ describe 'Student' do
   it 'creates a new instance of the Student class' do
     student = Student.new({name: "Morgan", age: 21})
 
-    expect student.to be_an_instance_of Student
+    expect(student).to be_an_instance_of Student
+  end
+
+  it 'takes in student info and has methods to report name and age' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.name).to eq "Morgan"
+    expect(student.age).to eq 21
   end
 end
