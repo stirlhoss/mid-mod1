84136e26e46117f6ecd580bc64ef6cc87545c2f2
Stirling Hostetter <stirhoss@icloud.com>
2022-04-06 12:27:09 -0500

    add test for log_scores and grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 class Student
+  attr_reader :scores
 
   def initialize(student_info)
     @student_info = student_info
+    @scores = []
   end
 
   def name
@@ -12,5 +14,13 @@ class Student
 
   def age
     @student_info[:age]
+  end
+
+  def log_scores(score)
+    @scores << score
+  end
+
+  def grade
+    avg = (@scores.sum.to_f / @scores.length.to_f)
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -15,4 +15,30 @@ describe 'Student' do
     expect(student.name).to eq "Morgan"
     expect(student.age).to eq 21
   end
+
+  it 'gives student a score that is an empty array' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.scores).to eq []
+  end
+end
+
+describe 'log_scores' do
+  it 'takes an argument and puts them into the scores array' do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_scores(89)
+    student.log_scores(78)
+
+    expect(student.scores).to eq [89, 78]
+  end
+end
+
+describe 'grade' do
+  it 'averages the scores in @scores' do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_scores(89)
+    student.log_scores(78)
+
+    expect(student.grade).to eq 83.5
+  end
 end
